2cc04eb70f18d27d50ea416b105d13059ce9e36a
squeaky-bruce <bruce@squeaky.com>
2019-04-18 13:12:21 -0400

    Mobile nav fix

--- header.php
+++ header.php
@@ -15,16 +15,9 @@
     <div class="usda-badge"></div>
     <div class="usda-badge-mobile"></div>
   </div>
-  <div class="mobile-nav-icon">
-    <svg viewBox="0 0 30 25">
-      <line id="top" x1="0" y1="2.5" x2="30" y2="2.5"/>
-      <line id="middle" x1="0" y1="12.5" x2="30" y2="12.5"/>
-      <line id="bottom" x1="0" y1="22.5" x2="30" y2="22.5"/>
-    </svg>
-  </div>
   <nav>
     <ul class="hr-list">
-      <li><a href="mailto:sweetleaftea@casupport.com">Contact</a></li>
+      <li><a href="https://www.sweetleaftea.com/contact/" target="_blank">Contact</a></li>
     </ul>
   </nav>
 </header>
