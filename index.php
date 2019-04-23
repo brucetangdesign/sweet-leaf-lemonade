d3aaa464eec6c8bca91e0bd6d8629ace3d77f586
squeaky-bruce <bruce@squeaky.com>
2019-04-23 11:59:52 -0400

    Mobile slideshow started

--- index.php
+++ index.php
@@ -77,6 +77,12 @@ $page = "home-page";
       <div class="slide-content">
         <div class="slide-image-container" data-rellax-speed="3">
           <div class="slide-image"></div>
+          <div class="bottles-mobile">
+            <div class="mobile-bottle classic"></div>
+            <div class="mobile-bottle pom-blueberry hidden"></div>
+            <div class="mobile-bottle cran-lime hidden"></div>
+            <div class="mobile-bottle orange-mango hidden"></div>
+          </div>
         </div>
         <div class="slide-copy" data-rellax-speed="3">
           <h1 class="h1-sans">Bless Your<span class="larger-line">Tastebuds</span></h1>
